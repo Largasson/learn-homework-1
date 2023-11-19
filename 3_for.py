55d441d114124ea152e41ecb92857f6d1d0d68cb
Pavel Naydenov <pavel.snk@gmail.com>
2023-11-19 11:39:05 +0300

    Переделал цикл

--- 3_for.py
+++ 3_for.py
@@ -27,17 +27,18 @@ def main():
         {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
         {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]}]
     res = []
-    for i in range(len(lst)):
-        res.append(sum(lst[i]['items_sold']))
+    res_of_length = []
+    for product_dict in lst:
+        temp = sum(product_dict['items_sold'])
+        length_of_seq =  len(product_dict['items_sold'])
+        print(f"Суммарное количество продаж смартфона {product_dict['product']}: {temp}")
+        print(f"Среднее количество продаж смартфона {product_dict['product']}: {round(temp / length_of_seq)}")
+        res.append(temp)
+        res_of_length.append(length_of_seq)
 
-    print(f'Суммарное количество продаж смартфона iPhone 12: {res[0]}')
-    print(f'Среднее количество продаж смартфона iPhone 12: {round(res[0] / 12)}')
-    print(f'Суммарное количество продаж смартфона Xiaomi Mi11: {res[1]}')
-    print(f'Среднее количество продаж смартфона Xiaomi Mi11: {round(res[1] / 12)}')
-    print(f'Суммарное количество продаж смартфона Samsung Galaxy 21: {res[2]}')
-    print(f'Среднее количество продаж смартфона Samsung Galaxy 21: {round(res[2] / 12)}')
+    medieval_length = sum(res_of_length) / len(lst)
     print(f'Суммарное количество продаж всех смартфонов: {sum(res)}')
-    print(f'Ссреднее количество продаж всех смартфонов: {round(sum(res) / 12)}')
+    print(f'Ссреднее количество продаж всех смартфонов: {round(sum(res) / medieval_length)}')
 
 
 if __name__ == "__main__":
